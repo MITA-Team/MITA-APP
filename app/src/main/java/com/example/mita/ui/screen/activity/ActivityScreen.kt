470f3419947e38f020c5b1638722bd80cffe1f88
hafizzibrahim <hafizzibrahimm0@gmail.com>
2023-12-05 19:47:33 +0700

    Update Question Screen

--- app/src/main/java/com/example/mita/ui/screen/activity/ActivityScreen.kt
+++ app/src/main/java/com/example/mita/ui/screen/activity/ActivityScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
-fun ActivityScreen(modifier: Modifier = Modifier){
+fun ActivityScreen(
+    modifier: Modifier = Modifier
+){
     Text(text = "Ini Halaman Activity")
 }
---
+++ app/src/main/java/com/example/mita/ui/screen/question/QuestionScreen.kt
@@ -0,0 +1,26 @@
+package com.example.mita.ui.screen.question
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun QuestionScreen(
+    modifier: Modifier = Modifier
+){
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(top = 48.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun QuestionScreenPreview(){
+    QuestionScreen()
+}
