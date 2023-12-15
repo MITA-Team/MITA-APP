1b2115efceb13f49c99297ab8bc571c53d64c952
hafizzibrahim <hafizzibrahimm0@gmail.com>
2023-12-15 09:48:25 +0700

    Update Auth Register

--- app/src/main/java/com/example/mita/navigation/Screen.kt
+++ app/src/main/java/com/example/mita/navigation/Screen.kt
@@ -1,10 +1,26 @@
 package com.example.mita.navigation
 
-sealed class Screen (val route: String) {
-    object Home : Screen("home")
-    object Activity : Screen("activity")
-    object Profile : Screen("profile")
-    object Login : Screen("login")
-    object Register : Screen("register")
-    object Welcome : Screen("welcome")
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+
+sealed class Screen () {
+    object WelcomeScreen: Screen()
+    object LoginScreen: Screen()
+    object RegisterScreen: Screen()
+    object HomeScreen: Screen()
+    object ActivityScreen: Screen()
+    object ProfileScreen: Screen()
+    object QuestionScreen: Screen()
+    object ResultScreen: Screen()
+    object TherapyScreen: Screen()
+    object TermsAndConditionsScreen : Screen()
+}
+
+object MitaAppRouter{
+
+    private var currentScreen: MutableState<Screen> = mutableStateOf(Screen.LoginScreen)
+
+    fun navigateTo(destination : Screen){
+        currentScreen.value = destination
+    }
 }
--- app/src/main/java/com/example/mita/ui/screen/register/RegisterScreen.kt
+++ app/src/main/java/com/example/mita/ui/screen/register/RegisterScreen.kt
@@ -1,192 +1,160 @@
 package com.example.mita.ui.screen.register
 
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
+import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavHostController
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mita.R
+import com.example.mita.navigation.MitaAppRouter
 import com.example.mita.navigation.Screen
+import com.example.mita.navigation.SystemBackButtonHandler
+import com.example.mita.ui.component.ButtonComponent
+import com.example.mita.ui.component.CheckBoxComponent
+import com.example.mita.ui.component.ClickableLoginTextComponent
+import com.example.mita.ui.component.DividerTextComponent
+import com.example.mita.ui.component.HeadingTextComponent
+import com.example.mita.ui.component.NormalTextComponent
+import com.example.mita.ui.component.PasswordTextFieldComponent
+import com.example.mita.ui.component.TextFieldComponent
+import com.example.mita.ui.screen.register.data.RegisterUIEvent
+import com.example.mita.ui.screen.register.data.RegisterViewModel
 
 @Composable
-fun RegisterScreen(
-    navController: NavHostController,
-    modifier: Modifier = Modifier
-){
-    Box(modifier = modifier.fillMaxSize()){
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
+fun RegisterScreen(registerViewModel: RegisterViewModel = viewModel()) {
+    val context = LocalContext.current
+
+
+    SystemBackButtonHandler {
+        MitaAppRouter.navigateTo(Screen.LoginScreen)
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Surface(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(horizontal = 24.dp)
+                .background(Color.White)
+                .padding(28.dp)
         ) {
-            Text(
-                "Sign Up",
-                style = TextStyle(
-                    fontSize = 28.sp,
-                    fontFamily = null,
-                    fontWeight = FontWeight(500),
-                ),
+            Column(
                 modifier = Modifier
-                    .padding(bottom = 16.dp)
-            )
-
-            TextField(
-                value = "",
-                onValueChange = {},
-                label = {
-                    Text(
-                        text = "Alamat Email"
-                    )
-                },
-                singleLine = true,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-                    .border(1.dp, Color.LightGray, shape = RoundedCornerShape(8.dp)),
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Color.White,
-                    unfocusedContainerColor = Color.White,
-                    disabledContainerColor = Color.White,
-                )
-            )
-
-            TextField(
-                value = "",
-                onValueChange = {},
-                label = {
-                    Text(
-                        text = "Nama Pengguna"
-                    )
-                },
-                singleLine = true,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-                    .border(1.dp, Color.LightGray, shape = RoundedCornerShape(8.dp)),
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Color.White,
-                    unfocusedContainerColor = Color.White,
-                    disabledContainerColor = Color.White,
-                )
-            )
-
-            TextField(
-                value = "",
-                onValueChange = {},
-                label = {
-                    Text(
-                        text = "Kata Sandi"
-                    )
-                },
-                singleLine = true,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-                    .border(1.dp, Color.LightGray, shape = RoundedCornerShape(8.dp)),
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Color.White,
-                    unfocusedContainerColor = Color.White,
-                    disabledContainerColor = Color.White,
-                )
-            )
-
-            TextField(
-                value = "",
-                onValueChange = {},
-                label = {
-                    Text(
-                        text = "Konfirmasi Kata Sandi"
-                    )
-                },
-                singleLine = true,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-                    .border(1.dp, Color.LightGray, shape = RoundedCornerShape(8.dp)),
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Color.White,
-                    unfocusedContainerColor = Color.White,
-                    disabledContainerColor = Color.White,
-                )
-            )
-
-            TextField(
-                value = "",
-                onValueChange = {},
-                label = {
-                    Text(
-                        text = "Tanggal Lahir"
-                    )
-                },
-                singleLine = true,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-                    .border(1.dp, Color.LightGray, shape = RoundedCornerShape(8.dp)),
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Color.White,
-                    unfocusedContainerColor = Color.White,
-                    disabledContainerColor = Color.White,
-                )
-            )
-
-            TextField(
-                value = "",
-                onValueChange = {},
-                label = {
-                    Text(
-                        text = "Domisili"
-                    )
-                },
-                singleLine = true,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-                    .border(1.dp, Color.LightGray, shape = RoundedCornerShape(8.dp)),
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Color.White,
-                    unfocusedContainerColor = Color.White,
-                    disabledContainerColor = Color.White,
-                )
-            )
-
-            Button(onClick = { navController.navigate(Screen.Home.route) },
-                shape = MaterialTheme.shapes.large,
-                colors = ButtonDefaults.buttonColors(),
-                modifier = Modifier
-                    .padding(8.dp)
+                    .fillMaxSize()
+                    .background(Color.White)
             ) {
-
-                Text(
-                    "Sign Up"
+                NormalTextComponent(value = stringResource(id = R.string.app_name))
+                HeadingTextComponent(value = stringResource(id = R.string.create_account))
+                Spacer(modifier = Modifier.height(20.dp))
+                TextFieldComponent(
+                    labelValue = stringResource(id = R.string.first_name),
+                    painterResource = painterResource(id = R.drawable.baseline_person_24),
+                    keyboardType = KeyboardType.Text,
+                    onTextSelected = {
+                        registerViewModel.onEvent(RegisterUIEvent.FirstNameChanged(it))
+                    },
+                    errorStatus = registerViewModel.registerUIState.value.firstNameError,
+                    errorMsg = registerViewModel.registerUIState.value.firstNameMsgError
                 )
 
+                TextFieldComponent(
+                    labelValue = stringResource(id = R.string.last_name),
+                    painterResource = painterResource(id = R.drawable.baseline_person_24),
+                    keyboardType = KeyboardType.Text,
+                    onTextSelected = {
+                        registerViewModel.onEvent(RegisterUIEvent.LastNameChanged(it))
+
+                    },
+                    errorStatus = registerViewModel.registerUIState.value.lastNameError,
+                    errorMsg = registerViewModel.registerUIState.value.lastNameMsgError
+                )
+
+                TextFieldComponent(
+                    labelValue = stringResource(id = R.string.email),
+                    painterResource = painterResource(id = R.drawable.baseline_email_24),
+                    keyboardType = KeyboardType.Email,
+                    onTextSelected = {
+                        registerViewModel.onEvent(RegisterUIEvent.EmailChanged(it))
+
+                    },
+                    errorStatus = registerViewModel.registerUIState.value.emailError,
+                    errorMsg = registerViewModel.registerUIState.value.emailMsgError
+
+                )
+
+                PasswordTextFieldComponent(
+                    labelValue = stringResource(id = R.string.password),
+                    painterResource = painterResource(id = R.drawable.baseline_lock_24),
+                    onTextSelected = {
+                        registerViewModel.onEvent(RegisterUIEvent.PasswordChanged(it))
+                    },
+                    errorStatus = registerViewModel.registerUIState.value.passwordError,
+                    errorMsg = registerViewModel.registerUIState.value.passwordMsgError
+
+                )
+
+                CheckBoxComponent(value = stringResource(id = R.string.trim_continuing),
+                    onTextSelected = {
+                        MitaAppRouter.navigateTo(Screen.TermsAndConditionsScreen)
+                    }, onCheckedChange = {
+                        registerViewModel.onEvent(RegisterUIEvent.PrivacyPolicyCheckBoxClicked(it))
+                    }, errorMsg = registerViewModel.registerUIState.value.privacyPolicyAcceptedMsgError
+                )
+
+                Spacer(modifier = Modifier.height(40.dp))
+
+                ButtonComponent(
+                    value = stringResource(id = R.string.register), onButtonClicked = {
+                        registerViewModel.onEvent(RegisterUIEvent.SignUpButtonClicked)
+                        if (registerViewModel.registerUIState.value.errorMsg != null) {
+                            Toast.makeText(
+                                context,
+                                registerViewModel.registerUIState.value.errorMsg.toString(),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }, true
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                DividerTextComponent()
+
+                ClickableLoginTextComponent(
+                    value = stringResource(id = R.string.already_login),
+                    onTextSelected = {
+                        MitaAppRouter.navigateTo(Screen.LoginScreen)
+                    })
             }
         }
+
+        if (registerViewModel.registerInProgress.value) {
+            CircularProgressIndicator()
+        }
     }
+
+
 }
 
-//@Preview(showBackground = true)
-//@Composable
-//fun RegisterScreenPreview(){
-//    RegisterScreen()
-//}
+@Preview
+@Composable
+fun DefaultPreviewOfSignUpScreen() {
+    RegisterScreen()
+}
